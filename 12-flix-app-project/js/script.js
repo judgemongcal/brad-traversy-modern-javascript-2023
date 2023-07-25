5eb3fc34780da7edd34db8c315d62f15497cb0a8
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-07-25 17:36:01 +0800

    141 - Movie Details Page

--- 12-flix-app-project/js/script.js
+++ 12-flix-app-project/js/script.js
@@ -2,6 +2,7 @@ const global = {
     currentPage: window.location.pathname
 };
 
+
 // Displaying Popular Movies
 const DisplayPopularMovies = async () => {
     const { results } = await fetchAPIData('movie/popular');
@@ -75,6 +76,81 @@ const DisplayPopularShows = async () => {
     });
 };
 
+const displayMovieDetails = async () => {
+    const movieId = (window.location.search).split('=')[1];
+    console.log(movieId);
+
+    const movieDetails = await fetchAPIData(`movie/${movieId}`);
+
+    console.log(movieDetails);
+
+    const prodCompanies = [];
+
+    movieDetails.production_companies.forEach((company) => {
+        prodCompanies.push(company.name);
+    });
+    
+
+    const container = document.querySelector('#movie-details');
+    container.innerHTML = `
+    <div class="details-top">
+      <div>
+        ${
+            movieDetails.poster_path? 
+
+            `<img
+            src="https://image.tmdb.org/t/p/w500${movieDetails.poster_path}"
+            class="card-img-top"
+            alt="${movieDetails.original_title}"
+          /> ` :
+
+          `
+          <img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${movieDetails.original_title}"
+        />
+          `
+        }
+      </div>
+      <div>
+        <h2>${movieDetails.original_title}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${movieDetails.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Release Date: ${movieDetails.release_date}</p>
+        <p>
+          ${movieDetails.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+          ${movieDetails.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${movieDetails.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget:</span> $${movieDetails.budget.toLocaleString()}</li>
+        <li><span class="text-secondary">Revenue:</span> $${movieDetails.revenue.toLocaleString()}</li>
+        <li><span class="text-secondary">Runtime:</span> ${movieDetails.runtime} Minutes</li>
+        <li><span class="text-secondary">Status:</span> ${movieDetails.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">${movieDetails.production_companies.map((company) => 
+            `<span>${company.name}</span>`
+        ).join(', ')}</div>
+    </div>
+  </div>
+    `;
+
+
+
+
+};
+
 
 // Fetch Data from TMDB API
 
@@ -128,6 +204,7 @@ const init = () => {
 
         case '/12-flix-app-project/movie-details.html':
             console.log('movie details');
+            displayMovieDetails();
             break;
 
         case '/12-flix-app-project/tv-details.html':
