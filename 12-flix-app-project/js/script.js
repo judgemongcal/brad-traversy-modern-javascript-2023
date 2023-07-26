b91de73ddf5ba324484c50280be8eecdc77aee60
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-07-26 12:53:32 +0800

    143 - TV Shows Details Page

--- 12-flix-app-project/js/script.js
+++ 12-flix-app-project/js/script.js
@@ -150,6 +150,68 @@ const displayMovieDetails = async () => {
 };
 
 
+// Display TV Show Details
+
+const displayShowDetails = async () => {
+    const showID = (window.location.search).split('=')[1];
+    console.log(showID);
+    const show = await fetchAPIData(`tv/${showID}`);
+    console.log(show);
+
+    displayBackgroundImage('tv-show', show.backdrop_path);
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="details-top">
+    <div>
+        ${ show.poster_path?
+          `<img
+          show.poster_path? 
+          src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+          class="card-img-top"
+          alt="${show.name}" />`
+          :
+          `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="S${show.name}" />`
+
+        }
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed()} / 10
+      </p>
+      <p class="text-muted">Release Date: ${show.first_air_date}</p>
+      <p>
+        ${show.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+      <li>
+        <span class="text-secondary">Last Episode To Air:</span> Episode ${show.last_episode_to_air.episode_number} - ${show.last_episode_to_air.name}
+      </li>
+      <li><span class="text-secondary">Status:</span> ${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}</div>
+  </div>
+    `;
+
+  document.querySelector('#show-details').appendChild(div);
+};
+
+
 // Display Backdrop on Details Pages
 
 const displayBackgroundImage = (type, backgroundPath) => {
@@ -165,13 +227,12 @@ const displayBackgroundImage = (type, backgroundPath) => {
     overlayDiv.style.top = '0';
     overlayDiv.style.left = '0';
     overlayDiv.style.zIndex = '-1';
-    overlayDiv.style.opacity = '0.2';
+    overlayDiv.style.opacity = '0.1';
 
     console.log(overlayDiv);
 
     if(type === 'movie') {
         document.querySelector('#movie-details').appendChild(overlayDiv);
-        console.log(backgroundPath);
     } else {
         document.querySelector('#show-details').appendChild(overlayDiv);
     }
@@ -237,6 +298,7 @@ const init = () => {
 
         case '/12-flix-app-project/tv-details.html':
             console.log('tv details');
+            displayShowDetails();
             break;
 
         case '/12-flix-app-project/search.html':
