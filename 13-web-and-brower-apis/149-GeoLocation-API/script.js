b3473aaad6410fdd1a9d7d9796ef5c03d9ee0554
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-07-27 22:31:28 +0800

    Module 149 & 150

--- 13-web-and-brower-apis/149-GeoLocation-API/script.js
+++ 13-web-and-brower-apis/149-GeoLocation-API/script.js
@@ -1,4 +1,29 @@
 // getCurrentPosition()
+function curSuccess(pos){
+    console.log(pos.coords.latitude);
+    console.log(pos.coords.longitude);
+}
+function curError(err){
+    console.log(`Error: ${err.code} - ${err.message}`);
+}
+
+const curOptions = {
+    enableHighAccuracy: true, // use GPS if available
+    timeout: 5000, // time to wait before stopping to try to get location
+    maximumAge: 0 // To not use a cached position
+};
+
+navigator.geolocation.getCurrentPosition(curSuccess, curError, curOptions);
 
 
-// watchPosition()
+// watchPosition() 
+
+navigator.geolocation.watchPosition(watSuccess, watError);
+
+function watSuccess(pos){
+    console.log(pos);
+}
+
+function watError(err){
+    console.log(err);
+}
