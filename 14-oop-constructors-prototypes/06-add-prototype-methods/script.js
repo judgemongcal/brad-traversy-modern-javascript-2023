6d052fdc9e4bc070563ec544c5c71cc51ae8930b
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-08-01 21:44:10 +0800

    171 - Add Prototype Methods

--- 14-oop-constructors-prototypes/06-add-prototype-methods/script.js
+++ 14-oop-constructors-prototypes/06-add-prototype-methods/script.js
@@ -2,12 +2,22 @@ function Rectangle(name, width, height) {
   this.name = name;
   this.width = width;
   this.height = height;
-  this.area = function () {
-  	return this.width * this.height;
-  };
-}
+};
 
+Rectangle.prototype.area = function () {
+  return this.width * this.height;
+};
 
-const rect = new Rectangle('Rect', 10, 10);
+Rectangle.prototype.perimeter = function () {
+  return 2 * (this.width + this.height);
+};
 
-console.log(rect);
+Rectangle.prototype.isSquare = function () {
+  return this.width === this.height;
+};
+
+const rect = new Rectangle('Rect', 10, 15);
+const rect2 = new Rectangle('Rect 2', 30, 30);
+
+console.log(rect.area(), rect.perimeter(), rect.isSquare());
+console.log(rect2.area(), rect2.perimeter(), rect2.isSquare());
