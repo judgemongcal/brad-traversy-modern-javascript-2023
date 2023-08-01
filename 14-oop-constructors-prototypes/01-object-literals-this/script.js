934a30382ca5b9a2ad46b61318fea52222b6e67e
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-08-01 18:27:48 +0800

    166 - Object Literals & "this"

---
+++ 14-oop-constructors-prototypes/01-object-literals-this/script.js
@@ -0,0 +1,10 @@
+const rectangle = {
+    name : 'Rectangle 1',
+    width: 10,
+    height: 10,
+    area : function () {
+        return this.width * this.height;
+    }
+};
+
+console.log(rectangle.area());
