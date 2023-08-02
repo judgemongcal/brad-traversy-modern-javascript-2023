10876f2de39f2947db0884dba681926b1a3f28ad
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-08-02 17:26:51 +0800

    177 - Static Methods

--- 15-oop-classes-private-properties/177-static-methods/script.js
+++ 15-oop-classes-private-properties/177-static-methods/script.js
@@ -4,4 +4,17 @@ class Rectangle {
     this.height = height;
     this.width = width;
   }
+
+  area() {
+    return this.width * this.height;
+  }
+
+  static getClass() {
+    return 'Rectangle';
+  }
 }
+
+const rect = new Rectangle('Rect 1', 20, 20);
+console.log(rect.area());
+// console.log(rect.getClass());
+console.log(Rectangle.getClass());
