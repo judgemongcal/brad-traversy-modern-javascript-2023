2e1d3b2679a669def01a9bc6c746b82973460539
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-08-07 18:35:22 +0800

    207 & 208 - NPM Packages & ES Modules

--- 17-modules-and-tooling/app.js
+++ 17-modules-and-tooling/app.js
@@ -1,7 +1,27 @@
-const {capitalizeWords, makeMoney} = require('./utils');
-const Person = require('./Person');
-const person1 = new Person('Robert', 29);
+// const {capitalizeWords, makeMoney} = require('./utils');
+// const Person = require('./Person');
+// const person1 = new Person('Robert', 29);
 
-console.log(capitalizeWords('lorem ipsum'));
+// console.log(capitalizeWords('lorem ipsum'));
+// console.log(makeMoney(5000));
+// person1.greet();
+
+
+// const axios = require('axios');
+
+// async function getPost() {
+//     const res = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+//     console.log(res.data);
+// }
+
+// getPost();
+
+
+import {capitalizeWords, makeMoney} from "./modules/utils.js";
+import Person from "./modules/Person.js";
+
+console.log(capitalizeWords('hello everyone'));
 console.log(makeMoney(5000));
+
+const person1 = new Person ('John', 40);
 person1.greet();
